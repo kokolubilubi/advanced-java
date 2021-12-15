01625722a06899fa36e7d32a92b5e1b2143c1d91
Jack <kokolubilubi@gmail.com>
2021-12-15 20:05:34 +0100

    Changed hello message!

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,8 +1,9 @@
 package app;
 // cd /mnt/c/Users/Jack/advanced-java/Simple/src
+// s
 public class App{
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!!!");
     }
 }
