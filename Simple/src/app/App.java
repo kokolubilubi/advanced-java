85f3a61cdfd0a900ce332ee7f81378c9dd709603
Jack <kokolubilubi@gmail.com>
2021-12-15 19:43:05 +0100

    Created

---
+++ Simple/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+// cd /mnt/c/Users/Jack/advanced-java/Simple/src
+public class App{
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
