d9b9cccaa9915cd30f390d43e534b125cf099ac6
Jack <kokolubilubi@gmail.com>
2021-12-25 21:41:07 +0100

    Created

---
+++ guava-example/src/main/java/com/caveofprogramming/examples/guava_example/App.java
@@ -0,0 +1,30 @@
+package com.caveofprogramming.examples.guava_example;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Multiset<String> animals = HashMultiset.create();
+        
+        animals.add("cat");
+        animals.add("cat");
+        animals.add("dog");
+        
+        animals.forEach(System.out::println);
+        
+    }
+}
+
+
+
+
+
