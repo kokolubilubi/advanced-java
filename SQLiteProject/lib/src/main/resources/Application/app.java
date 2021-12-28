d32d2b82e34917589522efeeff59217eeeb6680d
Jack <kokolubilubi@gmail.com>
2021-12-28 18:54:38 +0100

    Created

---
+++ SQLiteProject/lib/src/main/resources/Application/app.java
@@ -0,0 +1,66 @@
+package Application;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class app {
+	public static void main(String[] args) throws ClassNotFoundException, 	SQLException {
+		
+		int[] ids = {0,1,2};
+		String[] names = {"Sue", "Bob", "Charley"};
+		
+		Class.forName("org.sqlite.JDBC");
+		System.out.println("Hello World!");
+		
+		String dbUrl = "jdbc:sqlite:people.db";
+		var conn =  DriverManager.getConnection(dbUrl);
+		System.out.println(conn);
+		
+		var stmt = conn.createStatement();
+		conn.setAutoCommit(false);
+		
+		var sql = "create table if not exists user (id integer primary key, name text not null)";
+		stmt.execute(sql);
+		
+		sql = "insert into user(id,name) values (?,?)";
+		var insertStmt = conn.prepareStatement(sql);
+		
+		for(int i=0; i< ids.length;i++ )
+		{
+			insertStmt.setInt(1,ids[i]);
+			insertStmt.setString(2,names[i]);
+			
+			insertStmt.executeUpdate();
+		}
+		
+		conn.commit();
+		
+		insertStmt.close();
+		
+		sql = "select id, name from user";
+		var rs = stmt.executeQuery(sql);
+		
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			
+			System.out.println(id + " : " + name);
+		}
+		
+		sql = "drop table user";
+		//stmt.execute(sql);
+		
+		stmt.close();
+		conn.close();
+	}
+}
+
+
+
+
+
+
+
+
+
+
