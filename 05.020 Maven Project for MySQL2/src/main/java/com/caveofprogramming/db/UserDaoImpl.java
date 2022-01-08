44f82c31f9f59ee8ec49cbef7a8ac6ab1e66f464
Jack <kokolubilubi@gmail.com>
2022-01-08 21:56:42 +0100

    Created

--- 05.020 Maven Project for MySQL2/src/main/java/com/caveofprogramming/db/UserDaoImpl.java
+++ 05.020 Maven Project for MySQL2/src/main/java/com/caveofprogramming/db/UserDaoImpl.java
@@ -99,7 +99,7 @@ var conn = Database.instance().getConnection();
 		try {
 			var stmt = conn.createStatement();
 			
-			var rs = stmt.executeQuery("select id, name from user ");
+			var rs = stmt.executeQuery("select id, name from user order by id");
 			while(rs.next()) {
 				
 				var id = rs.getInt("id");
